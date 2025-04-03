b0062df069760ddbb5429a152a6ccb9e843a8720
LianaV27 <144968889+LianaV27@users.noreply.github.com>
2025-04-03 06:13:53 +0000

    try different txt box

--- src/form.jsx
+++ src/form.jsx
@@ -1,69 +1,64 @@
 import 'bulma/css/bulma.min.css';
 import { Form, Icon, Button } from 'react-bulma-components';
-const [message, setMessage] = useState('');
 
 export function SubscriberForm() {
-    return <form onSubmit={event => {
-        event.preventDefault();
-        const formData = new FormData(event.target);
-        const formObject = Object.fromEntries(formData.entries());
-        console.log(formObject);
-    }}>
-      <Form.Field>
-        <Form.Label>Username</Form.Label>
-        <Form.Control>
-          <Form.Input
-            color="success"
-            name="username"
-          />
-          <Icon align="left" size="small">
-            <i className="fas fa-user" />
-          </Icon>
-          <Icon align="right" size="small">
-            <i className="fas fa-check" />
-          </Icon>
-        </Form.Control>
-        <Form.Help color="success">This username is available</Form.Help>
-      </Form.Field>
-
-      <Form.Field>
-        <Form.Label>Email</Form.Label>
-        <Form.Control>
-          <Form.Input
-            color="danger"
-            name="email"
-          />
-          <Icon align="left" size="small">
-            <i className="fas fa-envelope" />
-          </Icon>
-          <Icon align="right" size="small">
-            <i className="fas fa-exclamation-triangle" />
-          </Icon>
-        </Form.Control>
-      </Form.Field>
-
-      <Form.Field>
-        <Form.Label>Message</Form.Label>
-        <Form.Control>
-        <Form.Textarea
-          value={message}
-          onChange={(e) => {
-            return setMessage(e.target.value);
-          }}
+  return <form onSubmit={event => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const formObject = Object.fromEntries(formData.entries());
+    console.log(formObject);
+  }}>
+    <Form.Field>
+      <Form.Label>Username</Form.Label>
+      <Form.Control>
+        <Form.Input
+          color="success"
+          name="username"
         />
-        </Form.Control>
-      </Form.Field>
+        <Icon align="left" size="small">
+          <i className="fas fa-user" />
+        </Icon>
+        <Icon align="right" size="small">
+          <i className="fas fa-check" />
+        </Icon>
+      </Form.Control>
+      <Form.Help color="success">This username is available</Form.Help>
+    </Form.Field>
 
-      <Form.Field kind="group">
-        <Form.Control>
-          <Button color="link">Submit</Button>
-        </Form.Control>
-        <Form.Control>
-          <Button color="link" colorVariant="light">
-            Cancel
-          </Button>
-        </Form.Control>
-      </Form.Field>
+    <Form.Field>
+      <Form.Label>Email</Form.Label>
+      <Form.Control>
+        <Form.Input
+          color="danger"
+          name="email"
+        />
+        <Icon align="left" size="small">
+          <i className="fas fa-envelope" />
+        </Icon>
+        <Icon align="right" size="small">
+          <i className="fas fa-exclamation-triangle" />
+        </Icon>
+      </Form.Control>
+    </Form.Field>
 
-    </form>
+    <Field>
+      <Control>
+        <Textarea
+          placeholder="I am a textarea"
+        />
+      </Control>
+    </Field>
+
+    <Form.Field kind="group">
+      <Form.Control>
+        <Button color="link">Submit</Button>
+      </Form.Control>
+      <Form.Control>
+        <Button color="link" colorVariant="light">
+          Cancel
+        </Button>
+      </Form.Control>
+    </Form.Field>
+
+  </form>
 }
