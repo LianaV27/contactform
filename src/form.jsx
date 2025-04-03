28d3ff3ecee145abf62bc4c8f6025fd0319be003
LianaV27 <144968889+LianaV27@users.noreply.github.com>
2025-04-03 05:33:34 +0000

    remove msg and merge branches

--- src/form.jsx
+++ src/form.jsx
@@ -22,7 +22,7 @@ export function SubscriberForm() {
             <i className="fas fa-check" />
           </Icon>
         </Form.Control>
-        <Form.Help color="success">This username is available</Form.Help>
+       
       </Form.Field>
 
       <Form.Field>
