881e006330cd889c80505308f679e4a525a08d12
LianaV27 <144968889+LianaV27@users.noreply.github.com>
2025-04-03 05:28:21 +0000

    remove invalid email message

--- src/form.jsx
+++ src/form.jsx
@@ -39,6 +39,7 @@ export function SubscriberForm() {
             <i className="fas fa-exclamation-triangle" />
           </Icon>
         </Form.Control>
+      
       </Form.Field>
 
       <Form.Field>
